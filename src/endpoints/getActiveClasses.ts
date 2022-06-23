fe4ba3acf16fef78812e1e5f3369fb54b6820681
Sérgio Pereira Dias Júnior <99135090+Sergiopdias@users.noreply.github.com>
2022-06-23 11:29:43 -0300

    Class requests

--- src/endpoints/getActiveClasses.ts
+++ src/endpoints/getActiveClasses.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from "express";
 import { ClassDataBase } from "../data/ClassDataBase";
 
-export const getAllClasses = async (req: Request, res: Response): Promise<void> => {
+export const getActiveClasses = async (req: Request, res: Response): Promise<void> => {
     let errorCode = 500
     try {
-        const allClasses = new ClassDataBase()
 
-        const classes = await allClasses.getAll()
+        const activeClasses = new ClassDataBase()
+
+        const classes = await activeClasses.getActiveClasses()
         if (!classes.length) {
             errorCode = 404
-            throw new Error("Sorry, we couldn't find any class!");
+            throw new Error("Sorry, we couldn't find any active class!");
         }
 
         res.send(classes)
+
     } catch (error: any) {
         res.status(errorCode).send(error.message || "Internal error!")
     }

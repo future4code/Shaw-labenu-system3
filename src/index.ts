c5f4739651c4efb2db4cae64f249df8df8a1982d
Matheus Martinelle Barros <matheusmartinellibarros@gmail.com>
2022-06-23 22:45:00 -0300

    Docente (Falta tratar Skills e Verificações)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { getAllClasses } from "./endpoints/getAllClasses";
 import { createNewClass } from "./endpoints/createNewClass"
 import { getActiveClasses } from "./endpoints/getActiveClasses";
 import { changeClassModule } from "./endpoints/changeClassModule";
+import { InstructorController } from "./endpoints/InstructorController";
 
 //-- getAllClasses --//
 app.get("/class", getAllClasses)
@@ -13,3 +14,12 @@ app.get("/activeClasses", getActiveClasses)
 //-- changeClassModule --//
 app.put("/classes/:id", changeClassModule)
 
+/* INSTRUCTOR */
+
+const instructorController = new InstructorController()
+
+app.post("/instructor", instructorController.postCreateInstructor)
+
+app.get("/instructors", instructorController.getAllInstructor)
+
+app.put("/instructor/:id", instructorController.putChangeInstructorClass)
